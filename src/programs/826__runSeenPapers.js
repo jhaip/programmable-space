f650ce58c8a25532e6ffca2e1c962ee5481777f4
Jacob Haip <haipjacob@gmail.com>
2020-11-14 13:23:59 -0500

    add spaces between variables in 826

--- src/programs/826__runSeenPapers.js
+++ src/programs/826__runSeenPapers.js
@@ -1,7 +1,7 @@
 const { room, myId, scriptName, run } = require('../helpers/helper')(__filename);
 
 room.on(
-  `camera $cameraId sees paper $id at TL ($, $) TR ($, $) BR ($, $) BL ($, $) @ $time`,
+  `camera $cameraId sees paper $id at TL ( $ , $ ) TR ( $ , $ ) BR ( $ , $ ) BL ( $ , $ ) @ $time`,
   results => {
     room.cleanup();
     (results || []).forEach(result => {
