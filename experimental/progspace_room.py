f511c04d0dafc13b0c996f8c068206973ae0db2b
Jacob Haip <haipjacob@gmail.com>
2021-02-08 08:40:15 -0500

    read 20 bytes

--- experimental/progspace_room.py
+++ experimental/progspace_room.py
@@ -71,7 +71,7 @@ class Room:
     def listen_and_update_subscriptions(self):
         while self.uart_server.in_waiting > 0:
             self.debug("Bytes in waiting: {}".format(self.uart_server.in_waiting))
-            read_msg = self.uart_server.read()
+            read_msg = self.uart_server.read(nbytes=20)
             if read_msg is not None:
                 self.recv_msg_cache += read_msg.decode("utf-8")
                 self.debug("sub update: {}".format(self.recv_msg_cache))
