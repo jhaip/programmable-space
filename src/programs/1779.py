dff53cdc8568177ccc5ebefc4736630a73b7fdbe
Jacob Haip <haipjacob@gmail.com>
2021-02-22 21:23:13 -0500

    add 15 more RFID cards for circuitpython

---
+++ src/programs/1779.py
@@ -0,0 +1,12 @@
+import time
+import board
+import analogio
+from progspace_room import Room
+
+room = Room(use_debug=True)
+
+while True:
+    while room.connected():
+        room.cleanup()
+        room.claim('temp is {}'.format(5))
+        time.sleep(1)
