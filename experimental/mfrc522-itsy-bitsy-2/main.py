7d43400808644da374014fc8a297c58bb7bd03d9
Jacob Haip <haipjacob@gmail.com>
2021-03-22 21:51:18 -0400

    save button press code and node.js gamepad test

--- experimental/mfrc522-itsy-bitsy-2/main.py
+++ experimental/mfrc522-itsy-bitsy-2/main.py
@@ -3,12 +3,23 @@ import usb_hid
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
 from adafruit_hid.keycode import Keycode
+from digitalio import DigitalInOut, Direction, Pull
 import mfrc522
 
 last_value = "FIRST_VALUE_TO_BE_ERASED"
 no_value_count = 0
 kbd = Keyboard(usb_hid.devices)
 layout = KeyboardLayoutUS(kbd)
+
+switch1 = DigitalInOut(board.D10)
+switch1.direction = Direction.INPUT
+switch1.pull = Pull.UP
+switch2 = DigitalInOut(board.D11)
+switch2.direction = Direction.INPUT
+switch2.pull = Pull.UP
+switch3 = DigitalInOut(board.D12)
+switch3.direction = Direction.INPUT
+switch3.pull = Pull.UP
 
 def send_value(val):
     print("sending {}".format(val.lower()))
@@ -22,6 +33,12 @@ print("\nPlace card before reader to read from address 0x08\n")
 
 try:
     while True:
+        if not switch1.value:
+            print("button 1 pressed")
+        if not switch2.value:
+            print("button 2 pressed")
+        if not switch3.value:
+            print("button 3 pressed")
         # print("loop")
         no_value_count += 1
         if no_value_count > 2:
