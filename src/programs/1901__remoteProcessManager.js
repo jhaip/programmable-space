46f2f87380ce24370f8c3d73ceaa1f8961a63a88
Jacob Haip <haipjacob@gmail.com>
2021-05-17 23:58:03 -0400

    pass through id

--- src/programs/1901__remoteProcessManager.js
+++ src/programs/1901__remoteProcessManager.js
@@ -13,7 +13,8 @@ const path = require('path');
 const pkill = require('pkill');
 const os = require('os');
 const fs = require('fs');
-const { room, run, MY_ID_STR, checkServerConnection } = require('../helpers/helper')(__filename, "1901");
+var myArgs = process.argv.slice(2);
+const { room, run, MY_ID_STR, checkServerConnection } = require('../helpers/helper')(__filename, myArgs.length > 0 ? myArgs[0] : "1901");
 
 const MY_COMPUTER_NAME = process.env.PROG_SPACE_MY_COMPUTER_NAME || os.hostname();
 const MY_SCRIPT_NAME = 'my_remote_script.sh'
