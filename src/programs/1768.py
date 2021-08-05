d8020ffdb4534626a5659382d0867c0c4bd72c82
Jacob Haip <haipjacob@gmail.com>
2021-08-04 22:31:41 -0400

    save example code for magtag

--- src/programs/1768.py
+++ src/programs/1768.py
@@ -1,12 +1,22 @@
-import time
-import board
-import analogio
-from progspace_room import Room
-
-room = Room(use_debug=True)
-
-while True:
-    while room.connected():
-        room.cleanup()
-        room.claim('temp is {}'.format(5))
-        time.sleep(1)
+from adafruit_magtag.magtag import MagTag
+import re
+DATA_SOURCE = "http://192.168.1.34:5000/select?first=1&subscription=%5B%22reading%20list%20%24%20is%20%24title%20%24%20%24%22%5D"
+magtag = MagTag(url=DATA_SOURCE)
+magtag.network.connect()
+magtag.add_text(
+    text_position=(
+        (magtag.graphics.display.width // 2) - 1,
+        (magtag.graphics.display.height // 2) - 1,
+    ),
+    text_scale=3,
+    text_anchor_point=(0.5, 0.5),
+    is_data=False,
+)
+try:
+    value = magtag.fetch()
+    print("Response is", value)
+    title = re.search(r"title\":\"(.+)\"", value).group(1)
+    magtag.set_text("Read\n"+title, 0)
+except (ValueError, RuntimeError) as e:
+    print("Some error occured, retrying! -", e)
+magtag.exit_and_deep_sleep(60)
--- src/programs/1774.py
+++ src/programs/1774.py
@@ -1,12 +1,32 @@
+# SPDX-FileCopyrightText: 2017 Scott Shawcroft, written for Adafruit Industries
+#
+# SPDX-License-Identifier: Unlicense
 import time
-import board
-import analogio
-from progspace_room import Room
+from adafruit_magtag.magtag import MagTag
 
-room = Room(use_debug=True)
+magtag = MagTag()
+
+magtag.add_text(
+    text_position=(
+        50,
+        (magtag.graphics.display.height // 2) - 1,
+    ),
+    text_scale=3,
+)
+
+magtag.set_text("Hello World")
+
+button_colors = ((255, 0, 0), (255, 150, 0), (0, 255, 255), (180, 0, 255))
+button_tones = (1047, 1318, 1568, 2093)
 
 while True:
-    while room.connected():
-        room.cleanup()
-        room.claim('temp is {}'.format(5))
-        time.sleep(1)
+    for i, b in enumerate(magtag.peripherals.buttons):
+        if not b.value:
+            print("Button %c pressed" % chr((ord("A") + i)))
+            magtag.peripherals.neopixel_disable = False
+            magtag.peripherals.neopixels.fill(button_colors[i])
+            magtag.peripherals.play_tone(button_tones[i], 0.25)
+            break
+    else:
+        magtag.peripherals.neopixel_disable = True
+    time.sleep(0.01)
