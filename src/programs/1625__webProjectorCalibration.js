4e3d3c555d21501f0d59263a65ada5fe39c11195
Jacob Haip <haipjacob@gmail.com>
2021-09-07 22:17:28 -0400

    persist projector calibration and claim camera calibration for now

--- src/programs/1625__webProjectorCalibration.js
+++ src/programs/1625__webProjectorCalibration.js
@@ -8,8 +8,35 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('./src/files/frame-to-papers'))
 
+const savedDataLocation = path.join(__dirname, '..', 'files', 'webProjectorCailbration.txt')
 var availableCameras = [];
 var allCamerasCalibrationData = {};
+
+room.assert(`camera 1998 should calibrate to 0 0 1920 0 1920 1080 0 1080 on display 1993`)
+
+fs.readFile(savedDataLocation, 'utf8', function(err, contents) {
+  if (err) {
+    console.log("No saved data")
+    console.error(err);
+  } else {
+    console.log("loaded data:")
+    console.log(contents);
+    let savedAllCamerasCalibrationData = undefined;
+    try {
+      savedAllCamerasCalibrationData = JSON.parse(contents);
+    } catch(e) {
+      console.error(e);
+    }
+    if (savedAllCamerasCalibrationData) {
+      allCamerasCalibrationData = savedAllCamerasCalibrationData;
+      room.retractAll(`camera $ has projector calibration TL ($, $) TR ($, $) BR ($, $) BL ($, $) @ $`)
+      for (const [cameraId, cal] of Object.entries(allCamerasCalibrationData)) {
+        room.assert(`camera "${cameraId}" has projector calibration TL (${cal[0]}, ${cal[1]}) TR (${cal[2]}, ${cal[3]}) BR (${cal[4]}, ${cal[5]}) BL (${cal[6]}, ${cal[7]}) @ 0`)
+      }
+      room.flush();
+    }
+  }
+});
 
 app.get('/status', (req, res) => {
     res.status(200).send({
@@ -60,6 +87,10 @@ room.on(`camera $cameraId has projector calibration TL ($x1, $y1) TR ($x2, $y2) 
           }, {});
         }
         room.subscriptionPostfix();
+        fs.writeFile(savedDataLocation, JSON.stringify(allCamerasCalibrationData), function (err) {
+          if (err) return console.log(err)
+          console.log("The file was saved!");
+        });
     })
 
 room.on(`camera $cameraId frame at $frameUrl`,
