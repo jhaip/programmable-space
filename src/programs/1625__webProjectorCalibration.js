bf3e8ef1f3ea81f034beddf3a5cb9f3903d0e9bc
Jacob Haip <haipjacob@gmail.com>
2021-09-07 22:19:14 -0400

    add missing import

--- src/programs/1625__webProjectorCalibration.js
+++ src/programs/1625__webProjectorCalibration.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const { room, myId, scriptName, run } = require('../helpers/helper')(__filename);
+const fs = require('fs');
+const path = require('path');
 const request = require('request');
 const app = express();
 const port = 3021;
