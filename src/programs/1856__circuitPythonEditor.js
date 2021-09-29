81d4e4a715253eea381adffc4a62c6dc1e043bf9
Jacob Haip <haipjacob@gmail.com>
2021-09-28 23:07:27 -0400

    try write file sync to avoid os saving errors

--- src/programs/1856__circuitPythonEditor.js
+++ src/programs/1856__circuitPythonEditor.js
@@ -58,28 +58,34 @@ function saveCodeToRoom(newCode) {
 
 function saveCodeToBoard(newCode) {
   if (boardConnected) {
-    const smallDelay = 5;
-    fs.open(CODE_FILENAME, 'w+', function(err, fd) {
-      if (err) {
-        console.log("could not open file", err);
-        fs.close(fd, function() {
-          console.log("closed file after error");
-        });
-      } else {
-        setTimeout(() => {
-          fs.writeFile(fd, newCode, {flag: 'w', encoding: 'utf8'}, err => {
-            if (err) {
-              console.log("Error saving code!", err);
-            }
-            setTimeout(() => {
-              fs.close(fd, function() {
-                console.log("done saving.")
-              });
-            }, smallDelay);
-          });
-        }, smallDelay);
-      }
-    });
+    try {
+      const data = fs.writeFileSync(CODE_FILENAME, newCode, { flag: 'w+' })
+      console.log("done saving.")
+    } catch (err) {
+      console.error(err)
+    }
+    // const smallDelay = 5;
+    // fs.open(CODE_FILENAME, 'w+', function(err, fd) {
+    //   if (err) {
+    //     console.log("could not open file", err);
+    //     fs.close(fd, function() {
+    //       console.log("closed file after error");
+    //     });
+    //   } else {
+    //     setTimeout(() => {
+    //       fs.writeFileSync(fd, newCode, {flag: 'w', encoding: 'utf8'}, err => {
+    //         if (err) {
+    //           console.log("Error saving code!", err);
+    //         }
+    //         setTimeout(() => {
+    //           fs.close(fd, function() {
+    //             console.log("done saving.")
+    //           });
+    //         }, smallDelay);
+    //       });
+    //     }, smallDelay);
+    //   }
+    // });
   } else {
     console.log("cannot save to board because board not connected");
   }
