22f5058d08da7a937cd5c152ea9be1d7b0e0f3ea
Jacob Haip <haipjacob@gmail.com>
2021-11-20 14:07:11 -0500

    custom style list and add long polling

--- src/files/web-debugger/db2/index.js
+++ src/files/web-debugger/db2/index.js
@@ -1,12 +1,22 @@
+var longPollingActive = false;
 $refreshButton = document.getElementById("refresh-button");
 $selectInput = document.getElementById("select-input");
 $results = document.getElementById("results");
+$longpoll = document.getElementById("longpoll");
 
 $selectInput.value = "$source %fact";
 $refreshButton.onclick = (evt) => {
     evt.preventDefault();
     refresh();
 }
+$$longpoll.addEventListener('change', (event) => {
+    if (event.currentTarget.checked) {
+        longPollingActive = true;
+        refresh();
+    } else {
+        longPollingActive = true;
+    }
+  })
 
 function render(data) {
     if (!data || data.length === 0) {
@@ -27,16 +37,16 @@ function render(data) {
         Object.keys(dataBySource).forEach(function (source) {
             decodedDataHTML += `<h4>${source}</h4>`
             decodedDataHTML += dataBySource[source].map(function (data) {
-                return `<li><div class="val-type">${data[1]}</div></li>`
+                return `<li><div class="val-type">${source} ${data[1]}</div></li>`
             }).join('\n');
         })
     } else if (!!data[0].fact) {
         decodedDataHTML = data.map(function (datum) {
-            return `<li>${JSON.stringify(datum.fact[1])}</li>`
+            return `<li><div class="val-type">${datum.fact[1]}</div></li>`
         }).join("\n")
     } else {
         decodedDataHTML = data.map(function (datum) {
-            return `<li>${JSON.stringify(datum)}</li>`
+            return `<li><div class="val-type">${JSON.stringify(datum)}</div></li>`
         }).join("\n")
     }
     $results.innerHTML = decodedDataHTML;
@@ -48,6 +58,11 @@ async function refresh() {
     const response = await fetch(`/select?query=${encodeURIComponent(squery)}`);
     const myJson = await response.json();
     render(myJson);
+    if (longPollingActive) {
+        setTimeout(function () {
+            refresh();
+        }, 1000);
+    }
 }
 
 refresh();
