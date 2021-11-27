e1d5a6b99664d11c6a871d78ae3ffb1944feb01f
Jacob Haip <haipjacob@gmail.com>
2021-11-26 23:07:07 -0500

    skip running other boot programs

--- src/programs/1900__processManager.js
+++ src/programs/1900__processManager.js
@@ -7,8 +7,14 @@ const { room, myId, run, MY_ID_STR, getIdFromProcessName, getIdStringFromId } = 
 let nameToProcessIdCache = {};
 
 function runPaper(name) {
-  if (name === '390__initialProgramCode.js') {
-    console.error(`skipping running 390__initialProgramCode.js`)
+  if (
+    [
+      "390__initialProgramCode.js",
+      "1900__processManager.js",
+      "112__arucoEditorBoot.js",
+    ].indexOf(name) !== 0
+  ) {
+    console.error(`skipping running boot program`);
     return;
   }
   console.error(`making ${name} be running!`)
