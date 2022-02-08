142cb414ab801fad0791742132b2c3cfbf0e8dc2
Jacob Haip <jhaip@mit.edu>
2022-02-07 20:11:32 -0500

    prevent duplicate claims if arucos didn't change

--- src/programs/1636__arucoWebManager.js
+++ src/programs/1636__arucoWebManager.js
@@ -58,19 +58,43 @@ app.post('/map', (req, res) => {
   res.status(200).send('OK');
 })
 
+function distanceSquared(x1, y1, x2, y2) {
+  return Math.pow(x2-x1, 2)  + Math.pow(y2-y1, 2);
+}
+
+function tagMovedEnoughToBeDifferent(a, b) {
+  total_corner_distance_squared = (
+    distanceSquared(+a.x1, +a.y1, +b.x1, +b.y1)
+    + distanceSquared(+a.x2, +a.y2, +b.x2, +b.y2)
+    + distanceSquared(+a.x3, +a.y3, +b.x3, +b.y3)
+    + distanceSquared(+a.x4, +a.y4, +b.x4, +b.y4)
+  );
+  PER_CORNER_DISTANCE_DIFF_THRESHOLD = 5 // px
+  return total_corner_distance_squared > 4 * Math.pow(PER_CORNER_DISTANCE_DIFF_THRESHOLD, 2)
+}
+
 room.on(`camera $camId sees aruco $id at $x1 $y1 $x2 $y2 $x3 $y3 $x4 $y4 @ $t`,
     results => {
-        room.subscriptionPrefix(1);
         seenArucoTags = results || [];
         seenArucoTags.forEach((v, i) => {
-          seenArucoCountdown[v.id] = {...v, "countdown": N_FRAMES_UNTIL_DEATH+1};
-        })
+          seenArucoCountdown[v.id] = {
+            ...v,
+            "different": !seenArucoCountdown[v.id] || tagMovedEnoughToBeDifferent(seenArucoCountdown[v.id], v),
+            "countdown": N_FRAMES_UNTIL_DEATH + 1
+          };
+        });
+        let somethingChanged = false;
+        let claims = [];
         for (const arucoId in seenArucoCountdown) {
           seenArucoCountdown[arucoId].countdown -= 1;
           if (seenArucoCountdown[arucoId].countdown <= 0) {
             delete seenArucoCountdown[arucoId];
+            somethingChanged = true;
           } else if (arucoToProgramMap[arucoId]) {
             const r = seenArucoCountdown[arucoId];
+            if (r.different) {
+              somethingChanged = true;
+            }
             const currentTimeMs = (new Date()).getTime();
             // assume paper is 4 times the size of the aruco card, extended in the -X and -Y direction.
             // so we use (x2, y2) as the origin (the top right corner)
@@ -84,10 +108,16 @@ room.on(`camera $camId sees aruco $id at $x1 $y1 $x2 $y2 $x3 $y3 $x4 $y4 @ $t`,
             const py3 = +r.y2 + scaleH*(+r.y3 - +r.y2);
             const px4 = +r.x2 + scaleW*(+r.x1 - +r.x2) + scaleH*(+r.x3 - +r.x2);
             const py4 = +r.y2 + scaleW*(+r.y1 - +r.y2) + scaleH*(+r.y3 - +r.y2);
-            room.assert(`camera ${r.camId} sees paper ${arucoToProgramMap[arucoId]} at TL ( ${px1} , ${py1} ) TR ( ${px2} , ${py2} ) BR ( ${px3} , ${py3} ) BL ( ${px4} , ${py4} ) @ ${currentTimeMs}`)
+            claims.push(`camera ${r.camId} sees paper ${arucoToProgramMap[arucoId]} at TL ( ${px1} , ${py1} ) TR ( ${px2} , ${py2} ) BR ( ${px3} , ${py3} ) BL ( ${px4} , ${py4} ) @ ${currentTimeMs}`)
           }
         }
-        room.subscriptionPostfix();
+        if (somethingChanged) {
+          room.subscriptionPrefix(1);
+          claims.forEach((c) => room.assert(c));
+          room.subscriptionPostfix();
+        } else {
+          console.log("skipping claim because arucos didn't change enough")
+        }
     })
 
 room.on(`program $programId is aruco $arucoId`,
