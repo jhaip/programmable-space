097ec1f684c33b8bd45d68979a39067764910df7
Jacob Haip <jhaip@mit.edu>
2022-03-13 11:11:37 -0400

    add in pressed keys claims to 650

--- src/programs/650__keyboard.py
+++ src/programs/650__keyboard.py
@@ -39,14 +39,9 @@ shift_map = {
 def add_key(key, special_key):
     logging.info("{} - {}".format(key, special_key))
     timestamp = int(time.time()*1000.0)
-    claims = []
-    claims.append({"type": "retract", "fact": [
-        ["id", get_my_id_str()],
-        ["postfix", ""],
-    ]})
     if special_key:
         logging.info("ADDING SPECIAL KEY {}".format(special_key))
-        claims.append({"type": "claim", "fact": [
+        return {"type": "claim", "fact": [
             ["id", get_my_id_str()],
             ["id", "0"],
             ["text", "keyboard"],
@@ -57,10 +52,10 @@ def add_key(key, special_key):
             ["text", str(special_key)],
             ["text", "@"],
             ["integer", str(timestamp)],
-        ]})
+        ]}
     else:
         logging.info("ADDING KEY {}".format(key))
-        claims.append({"type": "claim", "fact": [
+        return {"type": "claim", "fact": [
             ["id", get_my_id_str()],
             ["id", "0"],
             ["text", "keyboard"],
@@ -70,10 +65,14 @@ def add_key(key, special_key):
             ["text", str(key)],
             ["text", "@"],
             ["integer", str(timestamp)],
-        ]})
-    batch(claims)
+        ]}
 
 def handle_key_event(e):
+    claims = []
+    claims.append({"type": "retract", "fact": [
+        ["id", get_my_id_str()],
+        ["postfix", ""],
+    ]})
     ctrl_held = keyboard.is_pressed('ctrl')
     shift_held = keyboard.is_pressed('shift')
     if e.event_type == 'down':
@@ -81,23 +80,37 @@ def handle_key_event(e):
             return
         if ctrl_held:
             if shift_held and e.name.isalpha():
-                add_key(None, 'C-{}'.format(e.name.upper()))
+                claims.append(add_key(None, 'C-{}'.format(e.name.upper())))
             elif shift_held and e.name in shift_map:
-                add_key(None, 'C-{}'.format(shift_map[e.name]))
+                claims.append(add_key(None, 'C-{}'.format(shift_map[e.name])))
             elif e.name == '−':
-                add_key(None, 'C--'.format(e.name))
+                claims.append(add_key(None, 'C--'.format(e.name)))
             else:
-                add_key(None, 'C-{}'.format(e.name))
+                claims.append(add_key(None, 'C-{}'.format(e.name)))
         elif e.name in special_keys:
-            add_key(None, e.name)
+            claims.append(add_key(None, e.name))
         elif shift_held and e.name.isalpha():
-            add_key(e.name.upper(), None)
+            claims.append(add_key(e.name.upper(), None))
         elif shift_held and e.name in shift_map:
-            add_key(shift_map[e.name], None)
+            claims.append(add_key(shift_map[e.name], None))
         elif e.name == '−':
-            add_key('-', None)
+            claims.append(add_key('-', None))
         else:
-            add_key(e.name, None)
+            claims.append(add_key(e.name, None))
+    keys_to_claim_when_pressed = ["up", "right", "down", "left", "space"]
+    for key_name in keys_to_claim_when_pressed:
+        if keyboard.is_pressed(key_name):
+            claims.append({"type": "claim", "fact": [
+                ["id", get_my_id_str()],
+                ["id", "0"],
+                ["text", "keyboard"],
+                ["text", get_my_id_str()],
+                ["text", "has"],
+                ["text", "pressed"],
+                ["text", "key"],
+                ["text", key_name],
+            ]})
+    batch(claims)
 
 keyboard.hook(handle_key_event)
 keyboard.wait()
